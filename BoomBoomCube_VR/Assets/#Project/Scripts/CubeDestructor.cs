98f318ecd542aaf77cf12ff46c6eb0de0c1514d1
superlambdauser <r.froment@interface3.be>
2025-08-29 15:01:31 +0200

    collision manager on the cube prefab

---
+++ BoomBoomCube_VR/Assets/#Project/Scripts/CubeDestructor.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CubeDestructor : MonoBehaviour
+{
+    [SerializeField] Collider handsCollider;
+    [SerializeField] Collider invisibleWallCollider;
+
+    [SerializeField] AudioSource crushingCubesAudio;
+    [SerializeField] AudioSource missedCubeAudio;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other == handsCollider)
+        {
+            crushingCubesAudio.Play();
+            // Controller vibration
+            // Call object method that reduces its size
+            Destroy(gameObject, 1);
+        }
+
+        if (other == invisibleWallCollider)
+        {
+            missedCubeAudio.Play();
+            Destroy(gameObject);
+        }
+    }
+}
